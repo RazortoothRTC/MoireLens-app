e01c13b01458bdbb6c033725e62177071c6e20eb
Jaime Andrade <92104947+jandradeco@users.noreply.github.com>
2022-06-13 12:59:36 -0500

    created filters

---
+++ Shared/MoireLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoireLensApp.swift
+//  Shared
+//
+//  Created by Delta on 24/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MoireLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
