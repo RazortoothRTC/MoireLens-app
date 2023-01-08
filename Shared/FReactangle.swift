83414fcae7ea31241884ce463e9c649fae5aadf0
Ernest Ibarolle <63222761+AdAstraCA@users.noreply.github.com>
2023-01-08 02:06:28 -0800

    Update FReactangle.swift

--- Shared/FReactangle.swift
+++ Shared/FReactangle.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct FReactngle: View{
     var isPreview = false
+    var color = 0.0
     var numShapes: Int
     
-    init(isPreview: Bool) {
+    init(isPreview: Bool, color: Double) {
         self.isPreview = isPreview
         self.numShapes = Constant.num / (isPreview ? 3 : 1)
+        self.color = color
     }
     
     var body: some View{
         ZStack {
             ForEach(Array(stride(from: 0, to: numShapes, by: 4)), id: \.self) { i in
                 Rectangle()
-                    .stroke(Color.white, lineWidth: Constant.borderWidth)
+                    .stroke(Color(white: color), lineWidth: Constant.borderWidth)
                     .frame(
                         width: Constant.minSizeShape + (CGFloat(i) * 4),
                         height: Constant.minSizeShape + (CGFloat(i) * 4),
