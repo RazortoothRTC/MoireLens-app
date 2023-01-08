8a55362c8d60c95eef72da7bd08d4e7244663932
Ernest Ibarolle <63222761+AdAstraCA@users.noreply.github.com>
2023-01-08 02:12:05 -0800

    Update Model.swift

--- Shared/Model.swift
+++ Shared/Model.swift
@@ -3,6 +3,7 @@
 //  MoireLens
 //
 //  Created by Delta on 13/6/22.
+//  Edited by Astra on 1/8/23.
 //
 
 import Foundation
@@ -10,4 +11,5 @@ import Foundation
 struct FilterModel {
     var filter: FILTERS
     var isPreview: Bool
+    var color: Double
 }
