24168fec7bd5d53dc4759754acbc9e7ff2e9381b
Ernest Ibarolle <63222761+AdAstraCA@users.noreply.github.com>
2023-01-08 01:55:29 -0800

    Update FCircle.swift

--- Shared/FCircle.swift
+++ Shared/FCircle.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct FCircle: View{
     var isPreview = false
+    var color = 1.0
     var numShapes: Int
     
-    init(isPreview: Bool) {
+    init(isPreview: Bool, color: Double) {
         self.isPreview = isPreview
         self.numShapes = Constant.num / (isPreview ? 3 : 1)
+        self.color = color
     }
     
     var body: some View{
             ZStack(alignment: .center){
                 ForEach(Array(stride(from: 0, to: numShapes, by: 4)), id: \.self) { i in
                     Circle()
-                        .stroke(Color.white, lineWidth: Constant.borderWidth)
+                        .stroke(Color(white: color), lineWidth: Constant.borderWidth)
                     .frame(
                         width: Constant.minSizeShape + (CGFloat(i) * 4),
                         height: Constant.minSizeShape + (CGFloat(i) * 4),
