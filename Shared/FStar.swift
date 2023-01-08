bd72d132d471faacac01e75aca85e8ed3b610a7e
Ernest Ibarolle <63222761+AdAstraCA@users.noreply.github.com>
2023-01-08 02:07:23 -0800

    Update FStar.swift

--- Shared/FStar.swift
+++ Shared/FStar.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct FStart: View{
     var isPreview = false
+    var color = 1.0
     var scale: Int
     
-    init(isPreview: Bool) {
+    init(isPreview: Bool, color: Double) {
         self.isPreview = isPreview
         self.scale = Constant.num
+        self.color = color
     }
     
     var body: some View{
         ZStack{
-            ForEach(0..<scale * 5) { i in
+            ForEach(0..<scale * 5, id: \.self) { i in
                 Rectangle()
-                    .fill(Color.white)
+                    .fill(Color(white: color))
                     .frame(width: Constant.borderWidth, height: 1000)
                     .rotationEffect(Angle(degrees: Double(90 + (i * 5)) ))
             }
